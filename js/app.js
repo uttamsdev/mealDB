8e407ef9a2a684f537c14d5e4305307f22b2ec38
UttamSaha <uttamsaha982@gmail.com>
2022-03-14 18:15:54 +0600

    project done

--- js/app.js
+++ js/app.js
@@ -2,8 +2,19 @@ const loadFoods = () => {
     document.getElementById('preloader').style.display = 'block';
     const inputField = document.getElementById('input-field');
     const inputFieldValue =inputField.value;
+    if(!inputFieldValue){
+        alert('Write something.............');
+        document.getElementById('preloader').style.display = 'none';
+        return;
+    }
+    else if(inputFieldValue < 0){
+        alert('Enter positive number');
+        document.getElementById('preloader').style.display = 'none';
+        return;
+    }
 
     document.getElementById('display-search-result').innerHTML = '';
+    document.getElementById('no-search-result').style.display = 'none';
     document.getElementById('main').style.display = "none";
     document.getElementById('footer').style.display = "none";
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFieldValue}`)
@@ -14,6 +25,10 @@ const loadFoods = () => {
 }
 
 const displayFoods = foods => {
+    if(!foods){
+        document.getElementById('no-search-result').style.display = 'block';
+        document.getElementById('preloader').style.display = 'none';
+    }
     console.log(foods);
     // document.getElementById('display-search-result').innerHTML = '';
     const searchResult = document.getElementById('display-search-result');
@@ -31,6 +46,7 @@ const displayFoods = foods => {
         searchResult.appendChild(div);
     })
     document.getElementById('preloader').style.display = 'none';
+    document.getElementById('no-search-result').style.display = 'none';
 }
 
 const loadFoodDetails = foodId => {
@@ -47,7 +63,7 @@ const getFoodDetails = food => {
          <img src="${food.strMealThumb}">
         <h4>${food.strMeal}</h4>
         <p>${food.strInstructions}</p>
-        <button><a href="${food.strYoutube}" target="_blank">Visit Youtube</a></button>
+        <button><a href="${food.strYoutube}" target="_blank">Watch in Youtube</a></button>
         
     </div>
 
