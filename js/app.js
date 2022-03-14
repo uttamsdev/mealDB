a7210e6f53272a782ca1b06a240beef47c92ab7b
UttamSaha <uttamsaha982@gmail.com>
2022-03-14 17:28:19 +0600

    description section added api

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const loadFoods = () => {
     document.getElementById('preloader').style.display = 'block';
     const inputField = document.getElementById('input-field');
     const inputFieldValue =inputField.value;
-    
+
     document.getElementById('main').style.display = "none";
     document.getElementById('footer').style.display = "none";
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFieldValue}`)
@@ -20,13 +20,34 @@ const displayFoods = foods => {
         const div = document.createElement('div');
         div.classList.add("search-foods")
         div.innerHTML = `
-        <img src="${food.strMealThumb}">
-        <h4>${food.strMeal}</h4>
-        <p>${food.strInstructions.slice(0,80)}</p>
+       <div onclick = "loadFoodDetails(${food.idMeal})">
+            <img src="${food.strMealThumb}">
+            <h4>${food.strMeal}</h4>
+            <p>${food.strInstructions.slice(0,80)}</p>
+       </div>
         `;
         searchResult.appendChild(div);
-        console.log('hello');
-        console.log(food.strMeal);
     })
     document.getElementById('preloader').style.display = 'none';
+}
+
+const loadFoodDetails = foodId => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
+    .then(response => response.json())
+    .then(data => getFoodDetails(data.meals[0]))
+}
+
+const getFoodDetails = food => {
+    console.log(food);
+    const foodDetails = document.getElementById('food-details');
+    foodDetails.innerHTML = `
+    <div>
+         <img src="${food.strMealThumb}">
+        <h4>${food.strMeal}</h4>
+        <p>${food.strInstructions}</p>
+        <button><a href="${food.strYoutube}" target="_blank">Visit Youtube</a></button>
+        
+    </div>
+
+    `;
 }
